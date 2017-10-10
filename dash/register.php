693168a180b47c13ff1bb9005d603eb1f68a0d83
Ryan Buxton <rbuxton1@gmail.com>
2017-10-10 09:35:42 -0600

    Got learnt

--- dash/register.php
+++ dash/register.php
@@ -1,26 +1,32 @@
 <? php
   include("config.php");
   session_start();
-  if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['username'];
-    $pass = $_POST['password'];
-    $comPass = $_POST['password2'];
-    $error = '';
 
-    $sql = "SELECT * from user where username = '$username'";
+  $msg = "";
+
+  if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = mysqli_real_escape_string($db,$_POST['username']);
+    $pass = mysqli_real_escape_string($db,$_POST['password']);
+    $comPass = mysqli_real_escape_string($db,$_POST['password2']);
+
+    $sql = "SELECT * from user where email = '$username'";
     $result = mysqli_query($db,$sql);
     if(0 != mysqli_num_rows($result)){
-      $error .= "Username already used.";
+      $msg .= "Username already used.";
       //Add test to see if its an email
     } else{
       if(pass != comPass){
-        $error .= "Passwords do not match.";
+        $msg .= "Passwords do not match.";
       } else {
-        $hashPass = hash($password);
+        $hashPass = hash('sha256', $password);
         $sql = "INSERT INTO user ('id', 'email', 'password') values (NULL, '$username', '$hashPass')"
+        $result = mysqli_query($db,$sql);
+        if($result){
+          $msg = "succ";
+        }
       }
     }
-    echo $error;
+    echo $msg;
     echo $username . ' ' . $pass .' ' . $sql;
   }
 ?>
