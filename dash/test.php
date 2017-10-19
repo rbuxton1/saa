a7a8d7c72691b9803578fdde9a4440a3bf286465
Ryan Buxton <rbuxton1@gmail.com>
2017-10-19 09:14:05 -0600

    test5

--- dash/test.php
+++ dash/test.php
@@ -1,16 +1,13 @@
 <?php
   include("config.php");
 
+
+
   if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 
-  $result;
   $sql = "INSERT INTO user (id, email, password) values (NULL, 'squee', 'scmhee')"
   $result = mysqli_query($db,$sql);
-  if($result){
-    echo "yeah";
-  } else{
-    echo "nah";
-  }
+  echo $result;
 ?>
