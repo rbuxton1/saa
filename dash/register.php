9d7ad272706c3d72c38e35667d57b8cd150e05ad
Ryan Buxton <rbuxton1@gmail.com>
2017-10-19 09:37:56 -0600

    Yeah

--- dash/register.php
+++ dash/register.php
@@ -23,7 +23,7 @@
         $sql = "INSERT INTO user (id, email, password) values (NULL, '$username', '$hashPass')";
         $result = mysqli_query($db,$sql);
         if($result){
-          $msg = "succ";
+          $msg .= "succ";
         }
       }
     }
