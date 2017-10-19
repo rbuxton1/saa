a3c401f975db962f1663d3d0469667ac7219ece8
Ryan Buxton <rbuxton1@gmail.com>
2017-10-19 08:58:05 -0600

    Test still

--- dash/test.php
+++ dash/test.php
@@ -3,6 +3,8 @@
   $sql = "INSERT INTO user (id, email, password) values (NULL, 'squee', 'scmhee')"
   $result = mysqli_query($db,$sql);
   if($result){
-    $msg = "succ";
+    echo "yeah";
+  } else{
+    echo "nah";
   }
 ?>
