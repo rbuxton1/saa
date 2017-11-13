f151a02d815d64da29cc104ee040639fc938ebe9
Ryan Buxton <rbuxton1@gmail.com>
2017-11-13 09:00:39 -0700

    debugging3

--- dash/test2.php
+++ dash/test2.php
@@ -1,7 +1,7 @@
 <?php
   include("config.php");
 
-  $hashPass = hash("stank");
+  $hashPass = hash('sha256', "stank");
   $sql = "INSERT INTO user (id, email, password) values (NULL, 'cheez', '$hashPass')";
   $result = mysqli_query($db,$sql);
   if($result){
