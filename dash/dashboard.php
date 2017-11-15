fc5ed6e7c0220b7fa1a106d1fadca78518b823f9
Ryan Buxton <rbuxton1@gmail.com>
2017-11-15 09:05:59 -0700

    Upload init

--- dash/dashboard.php
+++ dash/dashboard.php
@@ -17,9 +17,42 @@
       <p>
         Click <a href="logout.php">here</a> to log out. <br>
         <br>
-        Include upload form here
+
+        <form action="" method="post" enctype="multipart/form-data">
+          Select image to upload:
+          <input type="file" name="fileToUpload" id="fileToUpload">
+          <label>Title </label><input type="text" name="title" class="box">
+          <input type="submit" value="Upload Image" name="submit">
+        </form>
+        <?php
+          include('session.php');
+          include('config.php');
+
+          $name = $_SESSION['login_user'];
+          $title = $_POST['title'];
+
+          $target_dir = "uploads/";
+          $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+          $uploadOk = 1;
+          $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+          // Check if image file is a actual image or fake image
+          if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+            if($check !== false) {
+              echo "File is an image - " . $check["mime"] . ".";
+              $uploadOk = 1;
+
+              $sql = "INSERT INTO pendingArt (id, title, src, tags, rate, artist, data) VALUES (NULL, '$title', '$target_file', 'NO_TAG', NULL, '$name', NULL)";
+
+            } else {
+              echo "File is not an image.";
+              $uploadOk = 0;
+            }
+          }
+        ?>
+
       </p>
-      <hr
+      <hr>
       <p>
         Uploaded works by this artist here
       </p>
