3fa3aed5c7c9348188166c7664111bc145150533
Ryan Buxton <rbuxton1@gmail.com>
2017-11-15 09:40:36 -0700

    Okay

--- dash/dashboard.php
+++ dash/dashboard.php
@@ -48,6 +48,7 @@
               $result = mysqli_query($db,$sql);
 
               if($result){
+                move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)
                 echo 'Uploaded!';
               } else {
                 echo 'Failed: ' . (string($result));
@@ -55,17 +56,6 @@
             } else {
               echo "File is not an image.";
               $uploadOk = 0;
-            }
-
-            if ($uploadOk == 0) {
-              echo "Sorry, your file was not uploaded.";
-              // if everything is ok, try to upload file
-            } else {
-              if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-              } else {
-                echo "Sorry, there was an error uploading your file.";
-              }
             }
         ?>
 
