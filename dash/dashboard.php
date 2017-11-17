36063847d5476a1fd75c4d5dbda607ffd07f7a1c
Ryan Buxton <rbuxton1@gmail.com>
2017-11-17 09:32:58 -0700

    Table

--- dash/dashboard.php
+++ dash/dashboard.php
@@ -94,19 +94,22 @@
       <hr>
       <p>
         Uploaded works by this artist here (new)
-        <?php
-          $request = "SELECT * FROM pendingArt WHERE artist = '$user'";
-          $sql = mysqli_query($db, $request);
-          echo $request . " >> " . ((string) $sql);
-          while($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)){
-            echo "<tr>";
-            echo "<td><center>" . "<img src = '/uploads/'" . $row['src'] . "></img>" . "</center></td>";
-            echo "<td><center>" . $row['title'] . "</center></td>";
-            echo "<td><center>" . $row['tags'] . "</center></td>";
-            echo "<td><center>" . $row['data'] . "</center></td>";
-            echo "</tr>";
-          }
-        ?>
+        <table border="1">
+          <tr><th>Image</th> <th>Title</th> <th>Tags</th> <th>Extra Data</th></tr>
+            <?php
+              $request = "SELECT * FROM pendingArt WHERE artist = '$user'";
+              $sql = mysqli_query($db, $request);
+              echo $request . " >> " . ((string) $sql);
+              while($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)){
+                echo "<tr>";
+                echo "<td><center>" . "<img src = '/uploads/'" . $row['src'] . "></img>" . "</center></td>";
+                echo "<td><center>" . $row['title'] . "</center></td>";
+                echo "<td><center>" . $row['tags'] . "</center></td>";
+                echo "<td><center>" . $row['data'] . "</center></td>";
+                echo "</tr>";
+              }
+            ?>
+        </table>
       </p>
     </center>
   </body>
