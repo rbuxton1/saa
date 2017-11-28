af1c4f4926312533a9c66c0a64ab0ff880c6d243
Ryan Buxton <rbuxton1@gmail.com>
2017-11-28 09:53:28 -0700

    Line

--- dash/dashboard.php
+++ dash/dashboard.php
@@ -116,6 +116,7 @@
               }
             ?>
         </table>
+        <hr>
         <h2> Works waiting to be approved and rated by  <?php echo "(".$user.")"; ?> </h2> <br>
         <table border="1">
           <tr><th>Image</th> <th>Title</th> <th>Tags</th> <th>Extra Data</th></tr>
