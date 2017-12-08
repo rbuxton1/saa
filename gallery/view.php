8a867d15e8156b0443c3b5ed404c03a206733749
Ryan Buxton <rbuxton1@gmail.com>
2017-12-08 09:23:43 -0700

    Show artist

--- gallery/view.php
+++ gallery/view.php
@@ -13,15 +13,29 @@
           $sql = mysqli_query($db, $stmt);
           if($sql){
             $row = mysqli_fetch_array($sql, MYSQLI_ASSOC);
-            $imgSrc = $row['src'];
             echo("
               <table border=0>
                 <tr><img src = '../uploads/" . $row['src'] . "' style ='height:500px; width:auto;'></tr>
                 <tr><p><i>" . $row['title'] . " </i></p></tr>
                 <tr><p><i>By " . $row['artist'] ." </p></i></tr>
                 <tr><p>Description: <br> " . $row['data'] . " </p></tr>
-              ");
-            echo("<p>"($artist == null));
+              </table>");
+          }
+        }
+        if($artist != null){
+          $stmt = "SELECT * FROM liveArt WHERE artist = '$artist'";
+          $sql = mysqli_query($db, $stmt);
+          if($sql){
+            while($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)){
+              echo("
+                <table border=0>
+                  <tr><img src = '../uploads/" . $row['src'] . "' style ='height:500px; width:auto;'></tr>
+                  <tr><p><i>" . $row['title'] . " </i></p></tr>
+                  <tr><p><i>By " . $row['artist'] ." </p></i></tr>
+                  <tr><p>Description: <br> " . $row['data'] . " </p></tr>
+                </table>
+                <hr>");
+            }
           }
         }
 
