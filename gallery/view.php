df8dd1f4116946ae91584bb955f522d19c3f023d
Ryan Buxton <rbuxton1@gmail.com>
2017-12-08 09:18:35 -0700

    tes

--- gallery/view.php
+++ gallery/view.php
@@ -6,6 +6,7 @@
     <center>
       <?php
         $id = $_GET['id'];
+        $artist = $_GET['artist'];
 
         if($id != 0){
           $stmt = "SELECT * FROM liveArt WHERE id = '$id'";
@@ -13,17 +14,17 @@
           if($sql){
             $row = mysqli_fetch_array($sql, MYSQLI_ASSOC);
             $imgSrc = $row['src'];
-
+            echo("
+              <table border=0>
+                <tr><img src = '../uploads/" . $row['src'] . "' style ='height:500px; width:auto;'></tr>
+                <tr><p><i>" . $row['title'] . " </i></p></tr>
+                <tr><p><i>By " . $row['artist'] ." </p></i></tr>
+                <tr><p>Description: <br> " . $row['data'] . " </p></tr>
+              ");
+            echo($artist);
           }
-
-          echo("
-            <table border=0>
-              <tr><img src = '../uploads/" . $row['src'] . "' style ='height:500px; width:auto;'></tr>
-              <tr><p><i>" . $row['title'] . " </i></p></tr>
-              <tr><p><i>By " . $row['artist'] ." </p></i></tr>
-              <tr><p>Description: <br> " . $row['data'] . " </p></tr> 
-            ");
         }
+
       ?>
     </center>
   </body>
