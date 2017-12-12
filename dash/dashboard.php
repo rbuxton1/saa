ff3e3c4e4eb1c2773ed7ab1bdf992f64a7582472
Ryan Buxton <rbuxton1@gmail.com>
2017-12-12 09:47:24 -0700

    More formation

--- dash/dashboard.php
+++ dash/dashboard.php
@@ -33,7 +33,7 @@
             </tr><tr>
               <td><label>Title</label></td> <td><input type="text" name="title" class="box"></td>
             </tr><tr>
-              <td><label>Description</label></td> <td><input style="height:200px;" type="text" name="data" class="box"></td>
+              <td><label>Description</label></td> <td><input style="height:100px;" type="text" name="data" class="box"></td>
             </tr>
           </table>
           <input type="submit" value="Upload Image" name="submit">
