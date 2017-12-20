e058db81cd0e492faef1f1f03c7c2f49d3bd4206
Ryan Buxton <rbuxton1@gmail.com>
2017-12-20 16:54:30 -0700

    remove little menu boy

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 
     <!-- Header -->
     <header class="w3-container w3-theme w3-padding" id="myHeader">
-      <i onclick="w3_open()" class="fa fa-bars w3-xlarge w3-button w3-theme"></i>
       <div class="w3-center">
         <h4>STUDENT ART ARCHIVE</h4>
         <h1 class="w3-xxxlarge w3-animate-bottom">SAA <?php echo $version; ?></h1>
