a998084c8938b035c815fb50c7f9b6d6ba77e0e8
Ryan Buxton <rbuxton1@gmail.com>
2017-12-20 16:59:20 -0700

    tiny saa

--- index.php
+++ index.php
@@ -33,11 +33,12 @@
 
     <!-- Footer -->
     <footer class="w3-container w3-theme-dark w3-padding-16">
-      <h5>SAA, dude</h5>
       <p>
         <div class="w3-center">
           Student Art Archive version 1.0.0 &emsp; <a href="https://github.com/rbuxton1/saa/wiki">Built and maintained by Ryan Buxton and Curtis Worthy</a> &emsp; Senior Project 2017-2018 OHS <br>
           Theme by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
+          <br> <br>
+          <i> SAA, dude! </i>
         </div>
       </p>
       <div style="position:relative;bottom:55px;" class="w3-tooltip w3-right">
