0748a10a4b1c40eef9ac32fcdabe691bdede405a
Ryan Buxton <rbuxton1@gmail.com>
2017-12-21 10:56:59 -0700

    Debugging

--- gallery/gallery.php
+++ gallery/gallery.php
@@ -55,13 +55,14 @@
         $terms = "%" . $terms . "%";
         $req = "SELECT * FROM liveArt WHERE artist LIKE '" . $terms . "' OR title LIKE '" . $terms . "' OR data LIKE '" . $terms . "'";
         $sql = mysqli_query($db,$req);
-
+        echo "<p>" . $sql . "</p>";
         while($row = mysqli_fetch_array($sql, MYSQLI_ASSOC)){
           generateCard($row['id'], $row['src'], $row['title'], $row['artist'], $row['data']);
         }
       }
     ?>
 
+    <br>
     <!-- Footer -->
     <footer class="w3-container w3-theme-dark w3-padding-16">
       <p>
